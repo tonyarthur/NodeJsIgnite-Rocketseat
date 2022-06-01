630a85d3be5be525e0d56af5a699e9f230b3c490
Tony Arthur Santos Costa <tony.costa@baritecnologia.com.br>
2022-05-31 23:26:41 -0300

    Lendo dados e salvando

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,18 +1,56 @@
 import { parse as csvParse } from 'csv-parse';
 import fs from 'fs';
 
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
 class ImportCategoryUseCase {
+    constructor(private categoriesRepository: CategoriesRepository) { }
 
-    execute(file: Express.Multer.File): void {
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
 
-        const stream = fs.createReadStream(file.path);
+            const parseFile = csvParse();
 
-        const parseFile = csvParse();
+            stream.pipe(parseFile);
 
-        stream.pipe(parseFile);
+            parseFile.on("data", async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name,
+                    description,
+                });
 
-        parseFile.on("data", async (line) => {
-            console.log(line);
+            })
+                .on("end", () => {
+                    resolve(categories);
+                })
+                .on("error", (err) => {
+                    reject(err);
+                })
+        });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+
+        categories.map(async (category) => {
+            const { name, description } = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if (!existCategory) {
+                this.categoriesRepository.create({
+                    name,
+                    description,
+                });
+            }
         });
     }
 }
